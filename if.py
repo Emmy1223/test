f577207c91ecb0620b156ee016ab0d1c44ce41a2
Emmy1223 <faletiemmanuel28@gmail.com>
2026-04-28 10:08:16 +0100

    23

---
+++ if.py
@@ -0,0 +1,18 @@
+x = 4
+y = 24
+
+if x<10:
+    print ("X is less than 10")
+else:
+    print ("X is bigger than 10")
+
+
+for i in range(10,20,3):
+    print(i)
+
+fruit = ['apple', 'mango', 'watermelon']
+
+
+
+fruit.extend([1,2])
+print(fruit)
---
+++ main.py
@@ -0,0 +1,47 @@
+name = "Aderte yygfgyeghe"
+#age = 3
+#height = 6.7
+#is_active = True
+
+#print(type(name))
+#print(type(age))
+#print(type(height))
+#print(type(is_active))
+
+
+#print(type(str(age)))
+
+
+
+#A,B,C,D = 1,2,3,4
+#print(A)
+#print(B)
+#print(C)
+#print(D)
+
+#e=f=z = 0
+
+#print(e)
+#print(f)
+#print(z)
+
+x = 23
+y = 15
+z =5
+
+
+#print(not(x>y or x<z))
+
+x -= 3
+names = 'dert'
+
+#print(name.split())
+
+#print(f"hello, how are you {name} i am {x} years old")
+
+#print (f'{name}'+ ' '  + 'hello')
+
+name =  "hello name's are hfj gdhjd"
+namee = "hello name\"s are hfj gdhjd"
+
+print(namee)
